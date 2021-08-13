c4c2f0c7c9283b9a1ffe30b03f0f0c2e2929da3d
林海峰 <linhf@zts.com.cn>
2021-08-13 17:05:56 +0800

    Add virtual studio project

--- DogeUtility.h
+++ DogeUtility.h
@@ -7,6 +7,12 @@
 
 #define GUID_LEN 64
 #define MAX_PATH          260
+#ifdef _WIN32
+    #define DOGE_EXPORT __declspec(dllexport)
+#else
+    #define DOGE_EXPORT
+#endif // _WIN32
+
 
 namespace Doge_Utility
 {
@@ -15,10 +21,10 @@ namespace Doge_Utility
 	//bool __declspec(dllexport) deleteFile(const TCHAR* _pFrom, WORD flags = FOF_ALLOWUNDO | FOF_NOCONFIRMATION);
 	//bool __declspec(dllexport) moveFile(const TCHAR *_pFrom, const TCHAR *_pTo, WORD flags = FOF_NOCONFIRMATION);
 
-	std::string utf8ToAnsi(const std::string & str);
-	std::string ansiToUtf8(const std::string & str);
-    std::wstring ansiToUtf16(const std::string& str);
-    std::string utf16ToAnsi(const std::wstring& wstr);
+	std::string DOGE_EXPORT utf8ToAnsi(const std::string & str);
+	std::string DOGE_EXPORT ansiToUtf8(const std::string & str);
+    std::wstring DOGE_EXPORT ansiToUtf16(const std::string& str);
+    std::string DOGE_EXPORT utf16ToAnsi(const std::wstring& wstr);
 
 //#ifdef UNICODE
 //	wstring __declspec(dllexport) getMyDocumentPath();// 获取“我的文档”路径  
@@ -27,13 +33,13 @@ namespace Doge_Utility
 //#endif // UNICODE
 
     // vector -> string
-    std::string vectorToString(std::vector<std::string> vec, std::string delimiter);
+    std::string DOGE_EXPORT vectorToString(std::vector<std::string> vec, std::string delimiter);
     // string -> vector
-    void stringToVector(std::vector<std::string>& vec, std::string str, std::string delimiter);
+    void DOGE_EXPORT stringToVector(std::vector<std::string>& vec, std::string str, std::string delimiter);
 
-    std::string getAppPath();
-    std::string generateUuid();
-    std::string getEndpoint(const std::string & protocol, const std::string& ip, const std::string& port);
+    std::string DOGE_EXPORT getAppPath();
+    std::string DOGE_EXPORT generateUuid();
+    std::string DOGE_EXPORT getEndpoint(const std::string & protocol, const std::string& ip, const std::string& port);
 
-    void sleep(uint32_t ms);
+    void DOGE_EXPORT sleep(uint32_t ms);
 }
